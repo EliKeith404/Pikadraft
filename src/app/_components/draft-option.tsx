a9db32f828327ce8d383e906564d2d241ab0c00b
EliKeith404 <ekbass44@gmail.com>
2024-03-24 23:26:37 -0400

    fix: Disabled ability to highlight card text and drag images

--- src/app/_components/draft-option.tsx
+++ src/app/_components/draft-option.tsx
@@ -13,10 +13,10 @@ type DraftPokemonProps = {
 const DraftOption = (props: DraftPokemonProps) => {
   return (
     <div className="rounded-xl from-purple-500 to-blue-500 p-1 shadow hover:cursor-pointer hover:bg-gradient-to-tr peer-data-[state=checked]:bg-gradient-to-tr peer-data-[state=checked]:from-pink-500 peer-data-[state=checked]:to-red-500">
-      <div className="bg-card text-card-foreground rounded-xl p-4">
+      <div className="rounded-xl bg-card p-4 text-card-foreground">
         <div className="flex h-64 w-44 flex-col items-center justify-center gap-1 ">
           {/* NAME */}
-          <h3 className="font-semibold leading-none tracking-tight">
+          <h3 className="select-none font-semibold leading-none tracking-tight">
             {props.name}
           </h3>
           {/* TYPE ICONS */}
@@ -24,7 +24,7 @@ const DraftOption = (props: DraftPokemonProps) => {
             {props.typeIconUrls.map((url, i) => (
               <Image
                 key={i}
-                className="select-none"
+                className="pointer-events-none select-none"
                 width={32}
                 height={14}
                 src={url}
@@ -33,11 +33,13 @@ const DraftOption = (props: DraftPokemonProps) => {
             ))}
           </div>
           {/* TIER */}
-          <h4 className="text-muted-foreground mb-6 text-sm">{props.tier}</h4>
+          <h4 className="mb-6 select-none text-sm text-muted-foreground">
+            {props.tier}
+          </h4>
           {/* SPRITE */}
           <div className="relative flex h-40 w-32 items-center justify-center">
             <Image
-              className="select-none object-contain"
+              className="pointer-events-none select-none object-contain"
               priority
               fill
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
